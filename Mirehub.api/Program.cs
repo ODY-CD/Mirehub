2f1a7d8a3c77fdaed0e624f20af4b7b3dd68b951
Antony CANUT <canut42@gmail.com>
2022-03-24 15:07:00 +0100

    add swagger ui in release

--- Mirehub.api/Program.cs
+++ Mirehub.api/Program.cs
@@ -29,11 +29,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+// todo: remove if it works
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
